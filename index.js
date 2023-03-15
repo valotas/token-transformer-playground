e63ac82735e1fd2721afb4b7782bde4cc2b0fc5f
Georgios Valotasios <georgios.valotasios@fintama.com>
2023-03-15 12:27:36 +0100

    feat: introduce more complex tokens

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import { transformTokens } from "token-transformer";
-import { readJson } from "./lib/json.js";
+import { readJson, writeJsonToTemp } from "./lib/json.js";
 
 const tokensFile = process.argv[2];
 console.log("Processing tokens file:", tokensFile);
 
 const tokens = await readJson(tokensFile);
-const transformedTokens = transformTokens(tokens);
+const setsToUse = [];
+const excludes = [];
+const transformedTokens = transformTokens(tokens, setsToUse, excludes, {
+  expandTypography: true,
+  expandShadow: true,
+  expandComposition: true,
+  preserveRawValue: false,
+  throwErrorWhenNotResolved: true,
+  resolveReferences: true,
+});
 console.log("Transformed tokens:\n");
 console.dir(transformedTokens, { depth: null });
+
+const tmpJsonFile = await writeJsonToTemp(transformedTokens);
+console.log("Transformed tokens written to:", tmpJsonFile);
